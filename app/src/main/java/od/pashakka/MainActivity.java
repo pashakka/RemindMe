0590db79ec018cf086be9682e3c651c91bf535ff
pashakka <pashakka@gmail.com>
2017-02-19 12:26:33 +0200

    Create MainActivity class and layout

---
+++ app/src/main/java/od/pashakka/MainActivity.java
@@ -0,0 +1,18 @@
+package od.pashakka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import od.pashakka.remindme.R;
+
+/**
+ * Created by pasha on 19.02.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
