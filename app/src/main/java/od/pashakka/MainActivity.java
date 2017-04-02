a115566bec0ee4eb9d90b7a15d0d3f7888fabbcf
pashakka <pashakka@gmail.com>
2017-04-02 22:36:22 +0300

    Create MainActivity class and layout

--- app/src/main/java/od/pashakka/MainActivity.java
+++ app/src/main/java/od/pashakka/MainActivity.java
@@ -5,10 +5,6 @@ import android.os.Bundle;
 
 import od.pashakka.remindme.R;
 
-/**
- * Created by pasha on 19.02.2017.
- */
-
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
